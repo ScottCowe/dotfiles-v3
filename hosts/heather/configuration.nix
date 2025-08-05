16b7bd2641690b6c02a6690961f42d0f9cb69d5d
Scott Cowe <scott.t.cowe@gmail.com>
2025-08-05 21:41:19 +0100

    take 2

--- hosts/heather/configuration.nix
+++ hosts/heather/configuration.nix
@@ -25,14 +25,26 @@
       ipv4.addresses = [
         {
           address = "65.108.219.172";
+          prefixLength = 24;
         }
       ];
 
       ipv6.addresses = [
         {
           address = "2a01:4f9:c012:6a09::1";
+          prefixLength = 64;
         }
       ];
+    };
+
+    defaultGateway = {
+      address = "172.31.1.1";
+      interface = "eth0";
+    };
+
+    defaultGateway6 = {
+      address = "fe80::1";
+      interface = "eth0";
     };
   };
 
