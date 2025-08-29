5fa6078d89a12b443a7b2a81d1e30ad03ff52ff4
Scott Cowe <scott.t.cowe@gmail.com>
2025-08-29 23:45:59 +0100

    updated selkie disk config

--- hosts/selkie/disk-config.nix
+++ hosts/selkie/disk-config.nix
@@ -1,29 +1,30 @@
 {
-  disko.devices.disk.os = {
-    device = "/dev/sda";
-    type = "disk";
-    content = {
-      type = "gpt";
-      partitions = {
-        boot = {
-          type = "EF02";
-          size = "1M";
-        };
-        ESP = {
-          type = "EF00";
-          size = "1G";
-          content = {
-            type = "filesystem";
-            format = "vfat";
-            mountpoint = "/boot";
-          };
-        };
-        root = {
-          size = "100%";
-          content = {
-            type = "filesystem";
-            format = "ext4";
-            mountpoint = "/";
+  disko.devices = {
+    disk = {
+      main = {
+        device = "/dev/disk/by-id/";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size = "100M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
           };
         };
       };
