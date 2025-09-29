1f18a2be0b7a7d9f8c332a5ea6a21850546be2af
Scott Cowe <scott.t.cowe@gmail.com>
2025-09-29 11:14:55 +0100

    404ing cowe.dev until i have time to fix

--- hosts/heather/nginx.nix
+++ hosts/heather/nginx.nix
@@ -21,10 +21,16 @@
       "cowe.dev" = {
         enableACME = true;
         forceSSL = true;
-        locations."/" = {
-          proxyPass = "http://127.0.0.1:8080";
-          tryFiles = "$uri $uri/ /index.html";
-        };
+        # locations."/frontend-6484481303f37e26_bg.wasm" = {
+        #   proxyPass = "http://127.0.0.1:8080";
+        # };
+        # locations."/frontend-6484481303f37e26.js" = {
+        #   proxyPass = "http://127.0.0.1:8080";
+        # };
+        # locations."/" = {
+        #   proxyPass = "http://127.0.0.1:8080";
+        #   tryFiles = "$uri $uri/ /index.html";
+        # };
         locations."/api" = {
           proxyPass = "http://127.0.0.1:3000";
         };
