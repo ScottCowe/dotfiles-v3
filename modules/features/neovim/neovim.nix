d0e9a169753e4d1ff7d9e8a6d997d43e8688cb0c
Scott Cowe <scott.t.cowe@gmail.com>
2026-04-11 20:13:36 +0100

    neovim: added a bunch of treesitter parsers

--- modules/features/neovim/neovim.nix
+++ modules/features/neovim/neovim.nix
@@ -19,48 +19,65 @@
           ];
 
           env.XDG_DATA_DIRS = toString (
-            pkgs.linkFarm "nvim-plugins" [
-              {
-                name = "nvim/site/pack/plugins/start/kanagawa-nvim";
-                path = pkgs.vimPlugins.kanagawa-nvim;
-              }
-              {
-                name = "nvim/site/pack/plugins/start/lualine.nvim";
-                path = pkgs.vimPlugins.lualine-nvim;
-              }
-              {
-                name = "nvim/site/pack/plugins/start/blink-cmp";
-                path = pkgs.vimPlugins.blink-cmp;
-              }
-              {
-                name = "nvim/site/pack/plugins/start/gitsigns.nvim";
-                path = pkgs.vimPlugins.gitsigns-nvim;
-              }
-              {
-                name = "nvim/site/pack/plugins/start/nvim-autopairs";
-                path = pkgs.vimPlugins.nvim-autopairs;
-              }
-              {
-                name = "nvim/site/pack/plugins/start/indent-blankline-nvim";
-                path = pkgs.vimPlugins.indent-blankline-nvim;
-              }
-              {
-                name = "nvim/site/pack/plugins/start/diagflow-nvim";
-                path = pkgs.vimPlugins.diagflow-nvim;
-              }
-              {
-                name = "nvim/site/pack/plugins/start/nvim-treesitter";
-                path = pkgs.vimPlugins.nvim-treesitter.withAllGrammars;
-              }
-              {
-                name = "nvim/site/pack/plugins/start/nvim-treesitter-parsers-lua";
-                path = pkgs.vimPlugins.nvim-treesitter-parsers.lua;
-              }
-              {
-                name = "nvim/site/pack/plugins/start/nvim-treesitter-parsers-nix";
-                path = pkgs.vimPlugins.nvim-treesitter-parsers.nix;
-              }
-            ]
+            pkgs.linkFarm "nvim-plugins" (
+              [
+                {
+                  name = "nvim/site/pack/plugins/start/kanagawa-nvim";
+                  path = pkgs.vimPlugins.kanagawa-nvim;
+                }
+                {
+                  name = "nvim/site/pack/plugins/start/lualine.nvim";
+                  path = pkgs.vimPlugins.lualine-nvim;
+                }
+                {
+                  name = "nvim/site/pack/plugins/start/blink-cmp";
+                  path = pkgs.vimPlugins.blink-cmp;
+                }
+                {
+                  name = "nvim/site/pack/plugins/start/gitsigns.nvim";
+                  path = pkgs.vimPlugins.gitsigns-nvim;
+                }
+                {
+                  name = "nvim/site/pack/plugins/start/nvim-autopairs";
+                  path = pkgs.vimPlugins.nvim-autopairs;
+                }
+                {
+                  name = "nvim/site/pack/plugins/start/indent-blankline-nvim";
+                  path = pkgs.vimPlugins.indent-blankline-nvim;
+                }
+                {
+                  name = "nvim/site/pack/plugins/start/diagflow-nvim";
+                  path = pkgs.vimPlugins.diagflow-nvim;
+                }
+                {
+                  name = "nvim/site/pack/plugins/start/nvim-treesitter";
+                  path = pkgs.vimPlugins.nvim-treesitter.withAllGrammars;
+                }
+              ]
+              ++ (map
+                (x: {
+                  name = "nvim/site/pack/plugins/start/nvim-treesitter-parsers-${x}";
+                  path = pkgs.vimPlugins.nvim-treesitter-parsers.${x};
+                })
+                [
+                  "lua"
+                  "nix"
+                  "rust"
+                  "c"
+                  "java"
+                  "typst"
+                  "python"
+                  "javascript"
+                  "typescript"
+                  "html"
+                  "css"
+                  "bash"
+                  "haskell"
+                  "agda"
+                  "markdown"
+                ]
+              )
+            )
           );
 
           env.XDG_CONFIG_HOME = toString (
